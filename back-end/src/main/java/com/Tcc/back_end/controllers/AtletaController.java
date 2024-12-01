7e686282465198309c17a2d1e95781e45c17de43
rafael caetano <rafaelkaetanodf@gmail.com>
2024-12-01 02:41:29 -0300

    editar jogador

--- back-end/src/main/java/com/Tcc/back_end/controllers/AtletaController.java
+++ back-end/src/main/java/com/Tcc/back_end/controllers/AtletaController.java
@@ -1,6 +1,7 @@
 package com.Tcc.back_end.controllers;
 
 import com.Tcc.back_end.model.Atleta;
+import com.Tcc.back_end.repository.AtletaRepository;
 import com.Tcc.back_end.services.AtletaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,15 +9,18 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/atletas")
 public class AtletaController {
 
     private final AtletaService atletaService;
+    private final AtletaRepository atletaRepository;
 
-    public AtletaController(AtletaService atletaService) {
+    public AtletaController(AtletaService atletaService, AtletaRepository atletaRepository) {
         this.atletaService = atletaService;
+        this.atletaRepository = atletaRepository;
     }
 
     @GetMapping
@@ -40,7 +44,22 @@ public class AtletaController {
 
     @PutMapping
     public ResponseEntity<?> atualizarAtleta(@Validated @RequestBody Atleta atleta) {
-        var response = atletaService.save(atleta);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
+        // Verifica se o e-mail já está registrado para outro atleta
+        Optional<Atleta> atletaEmail = atletaRepository.findByEmail(atleta.getEmail());
+        Optional<Atleta> atletaExistente = atletaRepository.findById(atleta.getIdAtleta());
+
+        if (atletaExistente.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Atleta não encontrado.");
+        }
+
+        // Verifica se o e-mail pertence a outro atleta
+        if (atletaEmail.isPresent() && !atletaEmail.get().getIdAtleta().equals(atleta.getIdAtleta())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("E-mail já registrado para outro atleta.");
+        }
+
+        // Atualiza os dados do atleta
+        Atleta atletaAtualizado = atletaService.save(atleta);
+        return ResponseEntity.status(HttpStatus.OK).body(atletaAtualizado);
     }
+
 }
